946dd16f5b3410661254c2c1876779fa67cee745
hoetaek <rhoetaek@gmail.com>
2024-10-31 15:18:34 +0900

    Quartz sync: Oct 31, 2024, 3:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "hoetaek.github.io/taeknology",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
